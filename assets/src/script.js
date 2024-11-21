a015c819a36ad4c15b4922f2eb8b922913e96830
Thalia Danielle <thaliadani2@gmail.com>
2024-11-21 18:41:06 -0300

    Add files via upload

--- assets/src/script.js
+++ assets/src/script.js
@@ -16,9 +16,7 @@ function verificarCadastro() {
     if (nome.length >= 3 && idade >= 18 && idade <= 120 && cpf.length === 11 && !isNaN(cpf) && sintomas.length >= 5 && sintomas.length <= 100) {
         sessionStorage.setItem('nome', nome);
         sessionStorage.setItem('idade', idade);
-        sessionStorage.setItem('cpf', cpf);
         sessionStorage.setItem('sintomas', sintomas);
-        window.location.href = './dados-paciente.html'
     } else {
         alert('ACESSO NEGADO: Verifique as informações.');
     }
@@ -33,12 +31,16 @@ const cpf = sessionStorage.getItem('cpf');
 const sintomas = sessionStorage.getItem('sintomas');
 
 //Imprimir dados do paciente
-if(nome && idade && cpf && sintomas){
-    document.getElementById('dados').innerHTML =`
+if (nome && idade && cpf && sintomas) {
+    document.getElementById('dados').innerHTML = `
     <p>> <strong>Nome:</strong> ${nome}</p>
     <p>> <strong>Idade:</strong> ${idade}</p>
     <p>> <strong>CPF:</strong> ${cpf}</p>
     <p>> <strong>Sintomas:</strong><br> ${sintomas}</p>`;
-}else{
-    document.getElementById('dados').innerHTML =`<p>Dados não encontrados.</p>`
+} else {
+    document.getElementById('dados').innerHTML = `<p>Dados não encontrados.</p>`
 }
+
+
+
+
