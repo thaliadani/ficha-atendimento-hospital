72d06cfce927f9ee714c4669fa11e8546c9d81ea
Thalia Danielle <thaliadani2@gmail.com>
2024-11-26 10:27:21 -0300

    Add files via upload

--- assets/src/script.js
+++ assets/src/script.js
@@ -10,15 +10,19 @@ function verificarCadastro() {
     const nome = document.getElementById('nome').value;
     const idade = document.getElementById('idade').value;
     const cpf = document.getElementById('cpf').value;
+    const tel = document.getElementById('tel').value;
+    const email = document.getElementById('email').value;
     const sintomas = document.getElementById('sintomas').value;
 
     //Armazenar valores
-    if (nome.length >= 3 && idade >= 18 && idade <= 120 && cpf.length === 11 && !isNaN(cpf) && sintomas.length >= 5 && sintomas.length <= 100) {
+    if (nome.length >= 3 && idade >= 18 && idade <= 120 && cpf.length === 11 && !isNaN(cpf) && tel.length===11 && !isNaN(tel) && sintomas.length >= 5 && sintomas.length <= 100) {
         sessionStorage.setItem('nome', nome);
         sessionStorage.setItem('idade', idade);
         sessionStorage.setItem('cpf', cpf);
+        sessionStorage.setItem('tel', tel);
+        sessionStorage.setItem('email', email);
         sessionStorage.setItem('sintomas', sintomas);
-        window.location.href = './dados-paciente.html'
+        window.location.href = './dados-paciente.html';
     } else {
         alert('ACESSO NEGADO: Verifique as informações.');
     }
@@ -30,43 +34,22 @@ function verificarCadastro() {
 const nome = sessionStorage.getItem('nome');
 const idade = sessionStorage.getItem('idade');
 const cpf = sessionStorage.getItem('cpf');
+const tel = sessionStorage.getItem('tel');
+const email = sessionStorage.getItem('email');
 const sintomas = sessionStorage.getItem('sintomas');
 
+
 //Imprimir dados do paciente
-if (nome && idade && cpf && sintomas) {
+if (nome && idade && cpf && tel && email && sintomas) {
     document.getElementById('dados').innerHTML = `
     <p>> <strong>Nome:</strong> ${nome}</p>
     <p>> <strong>Idade:</strong> ${idade}</p>
     <p>> <strong>CPF:</strong> ${cpf}</p>
-    <p>> <strong>Sintomas:</strong><br> ${sintomas}</p>`;
+    <p>> <strong>Cel:</strong> ${tel}</p>
+    <p>> <strong>Email:</strong> ${email}</p>
+    <p>> <strong>Sintomas:</strong><br> ${sintomas}</p>`
 } else {
     document.getElementById('dados').innerHTML = `<p>Dados não encontrados.</p>`
-}
-
-
-//Digitar apenas letras
-
-function lettersOnly(evt) {
-    evt = (evt) ? evt : event;
-    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
-        ((evt.which) ? evt.which : 0));
-    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
-        (charCode < 97 || charCode > 122)) {
-        return false;
-    }
-    return true;
-}
-
-//Digitar apenas numeros
-function numbersOnly(evt) {
-    evt = (evt) ? evt : event;
-    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
-        ((evt.which) ? evt.which : 0));
-    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
-        (charCode < 97 || charCode > 122)) {
-        return true;
-    }
-    return false;
 }
 
 
